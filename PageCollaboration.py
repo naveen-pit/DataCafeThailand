0641ea9c04c966004f711a58607be81c03b50b99
Naveen <naveen_reg@Outlook.com>
2017-09-11 00:19:31 +0700

    inject user_list in the query

--- PageCollaboration.py
+++ PageCollaboration.py
@@ -1,5 +1,6 @@
 import uuid
 from google.cloud import bigquery
+from MongoDB import MongoDBManager
 client = bigquery.Client()
 def query(query_string ):
     query_job = client.run_async_query(str(uuid.uuid4()), query_string)
@@ -11,7 +12,7 @@ def query(query_string ):
     for row in destination_table.fetch_data():
         data.append(row)
     return data
-def get_page_list():
+def get_page_dict():
     query_string = "SELECT user_id,page_url FROM [datacafethailand:social_insight.customer_connection_profile] where source = 'Facebook'"
     data = query(query_string)
     page_dict={}
@@ -19,40 +20,26 @@ def get_page_list():
         page_id = row[0]
         page_url = row[1]
         page_dict[page_id]=page_url
-        print(page_id)
-        print(page_url)
-def get_unique_users_list(page_id):
-    query_string = "SELECT user_id FROM [datacafethailand:a19.page_post_user] where page_id='"+page_id+"' group by user_id limit 5"
+    return page_dict
+def get_pages_interacted_with_users(page_id,page_dict):
+    query_string = """select page_id,count(*) as count from  
+        (SELECT user_id,page_id FROM [datacafethailand:a19.page_user_count] where user_id in 
+        (SELECT user_id FROM [datacafethailand:a19.page_post_user] where page_id='"""+page_id+"""' group by user_id)
+        )
+        group by page_id"""
     data = query(query_string)
-    page_dict={}
+    page_score = {}
     for row in data:
-        print(row)
+        page_id = row[0]
+        count = row[1]
+        page_url = page_dict[page_id]
+        page_score[page_id] ={'count':count,'page_url':page_url}
+    return page_score
 if __name__=="__main__":
-    get_page_list()
-    get_unique_users_list('129558990394402')
-# query_job = client.run_async_query(str(uuid.uuid4()), """
-#     #standardSQL
-#     SELECT corpus AS title, COUNT(*) AS unique_words
-#     FROM `publicdata.samples.shakespeare`
-#     GROUP BY title
-#     ORDER BY unique_words DESC
-#     LIMIT 10""")
-
-    
-
-# query_job.begin()
-# query_job.result()  # Wait for job to complete.
-# destination_table = query_job.destination
-# destination_table.reload()
-# page_dict={}
-# for row in destination_table.fetch_data():
-#     page_id = row[0]
-#     page_url = row[1]
-#     page_dict[page_id]=page_url
-#     print(page_id)
-#     print(page_url)
-
-
-    
-
-
+    mongo = MongoDBManager('PageCollab')
+    page_dict = get_page_dict()
+    page_id ='129558990394402' 
+    page_url = page_dict[page_id]
+    page_score = get_pages_interacted_with_users(page_id,page_dict) #khaosod
+    data = {'page_id':page_id,'page_url': page_url,'page_score':page_score}
+    mongo.insert_one('page_score',data)
