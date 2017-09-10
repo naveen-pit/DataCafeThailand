65e6c74a6c2f7c373b829fa99d02604efd2a11ed
Naveen <naveen_reg@Outlook.com>
2017-09-11 00:18:47 +0700

    - add MongoDB Manager

---
+++ MongoDB.py
@@ -0,0 +1,12 @@
+import pymongo
+from pymongo import MongoClient
+class MongoDBManager:
+    def __init__(self,database,server = 'localhost',port=27017):
+        self.client = MongoClient(server,port)
+        self.db = self.client[database]
+    def insert_one(self,collection,data):
+        collection = self.db[collection]
+        collection.insert_one(data)
+if __name__=="__main__":
+    mongo = MongoDBManager('PageCollab')
+    mongo.insert_one('test',{'test':1})
