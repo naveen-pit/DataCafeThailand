873989c0d1973cd12c7bab81caa0de106493b8c1
naveen_reg@outlook.com <naveen_reg@outlook.com>
2017-10-29 23:41:31 +0700

    show monthly data

--- PageCollaboration.py
+++ PageCollaboration.py
@@ -205,114 +205,138 @@ def extract_top_from_each_category(page_score_list):
     for page in page_score_list:
         page_id = page['page_id']
         page_score[page_id] = page
-        page_score[page_id]['brand']=[]
-        page_score[page_id]['media']=[]
-        page_score[page_id]['artist']=[]
-        score = page_score[page_id]['score']
-        score = score[:-2]
-        num_page_in_list =0
-        for page_tuple in reversed(score):            
-            if num_page_in_list == (num_top*3):
-                break
-            category = page_tuple[3]
-            if category==1:
-                if len(page_score[page_id]['brand'])<num_top:
-                    page_score[page_id]['brand'].append(page_tuple)
-                    num_page_in_list = num_page_in_list+1
-            elif category==2 or category==4 or category==5:
-                if len(page_score[page_id]['media'])<5:
-                    page_score[page_id]['media'].append(page_tuple)
-                    num_page_in_list = num_page_in_list+1
-            elif category==3:
-                if len(page_score[page_id]['artist'])<num_top:
-                    page_score[page_id]['artist'].append(page_tuple)
-                    num_page_in_list = num_page_in_list+1
+        for month in page_score[page_id]['score']:
+            page_score[page_id][month]={}
+            page_score[page_id][month]['brand']=[]
+            page_score[page_id][month]['media']=[]
+            page_score[page_id][month]['artist']=[]
+            score = page_score[page_id]['score'][month]
+            score = score[:-2]
+            num_page_in_list =0
+            for page_tuple in reversed(score):            
+                if num_page_in_list == (num_top*3):
+                    break
+                category = page_tuple[3]
+                if category==1:
+                    if len(page_score[page_id][month]['brand'])<num_top:
+                        page_score[page_id][month]['brand'].append(page_tuple)
+                        num_page_in_list = num_page_in_list+1
+                elif category==2 or category==4 or category==5:
+                    if len(page_score[page_id][month]['media'])<5:
+                        page_score[page_id][month]['media'].append(page_tuple)
+                        num_page_in_list = num_page_in_list+1
+                elif category==3:
+                    if len(page_score[page_id][month]['artist'])<num_top:
+                        page_score[page_id][month]['artist'].append(page_tuple)
+                        num_page_in_list = num_page_in_list+1
     return page_score 
 
 def generate_data_of_top(page_score):
-    node_list=[]
-    edge_list=[]
+    month_list = ['-1','1','2','3','4','5','6','7','8']
     category_list = ['brand','media','artist']
-    node_exist={}
-
-    for pageid1 in page_score:
-        for category in category_list:
-            for page_tuple in page_score[pageid1][category]:
-                score = page_tuple[0] *100
-                pageid2 = page_tuple[1]
-                category = page_tuple[3]
-                if (pageid1 not in node_exist) or (pageid2 not in node_exist[pageid1]):
-                    link={
-                        'source':pageid1,
-                        'target':pageid2,
-                        'value':score
-                    }
-                    if len(page_score[pageid1]['score'])>0 and page_score[pageid1]['category'] in CATEGORY and page_score[pageid2]['category'] in CATEGORY:
-                        edge_list.append(link)
-                    if (pageid1 not in node_exist):
-                        node_exist[pageid1]={}
-                    if (pageid2 not in node_exist):
-                        node_exist[pageid2]={}
-                    node_exist[pageid1][pageid2]=True
-                    node_exist[pageid2][pageid1]=True
-    g= nx.Graph()
-    for page_id in page_score:
-        if len(page_score[page_id]['score'])>0 and page_score[page_id]['category'] in CATEGORY:
-            g.add_node(page_id)
-    for edge in edge_list:
-        if len(page_score[edge['source']]['score'])>0 and page_score[edge['source']]['category'] in CATEGORY and page_score[edge['target']]['category'] in CATEGORY:
-            g.add_edge(edge['source'],edge['target'],weight=edge['value'])
-    partition = community.best_partition(g)
-    degree = nx.degree_centrality(g)
-    betweenness = nx.betweenness_centrality(g)
-    closeness = nx.closeness_centrality(g)
-    eigenvector = nx.eigenvector_centrality(g)
-    page_info_data={}
-    for page_id in page_score:
-        if len(page_score[page_id]['score'])>0 and page_score[page_id]['category'] in CATEGORY:
-            catetory_id = page_score[page_id]['category']
-            if catetory_id==2 or catetory_id==4 or catetory_id==5:
-                catetory_id=2
-            node = {
-                'id':page_id  
-            }
-            node_list.append(node)
-            page_info_data[page_id]={
-                'group':catetory_id,
-                'partition':partition[page_id],
-                'name':page_score[page_id]['name'],
-                'betweenness':betweenness[page_id],
-                'closeness':closeness[page_id],
-                'eigenvector':eigenvector[page_id],
-                'degree':degree[page_id],
-                'brand':page_score[page_id]['brand'],
-                'media':page_score[page_id]['media'],
-                'artist':page_score[page_id]['artist']
-            }
     category_data=[]
+    page_info_data={}
     for category_num in CATEGORY:
         if category_num==4 or category_num==5:
             category_num=2 #group cat4,5 to cat2
         category_num = category_num-1
         category_data.append(category_list[category_num])
     data={
-        'nodes':node_list,
-        'links':edge_list,
-        'page_info':page_info_data,
+        'nodes':{},
+        'links':{},
+        #'page_info':page_info_data,
         'category':list(set(category_data))
     }
+    for month in month_list:
+        print('month='+month)
+        node_list=[]
+        edge_list=[]
+        
+        node_exist={}
+
+        for pageid1 in page_score:
+            for category in category_list:
+                if month in page_score[pageid1] and category in page_score[pageid1][month]:
+                    for page_tuple in page_score[pageid1][month][category]:
+                        score = page_tuple[0] *100
+                        pageid2 = page_tuple[1]
+                        category = page_tuple[3]
+                        if (pageid1 not in node_exist) or (pageid2 not in node_exist[pageid1]):
+                            link={
+                                'source':pageid1,
+                                'target':pageid2,
+                                'value':score
+                            }
+                            if len(page_score[pageid1]['score'][month])>0 and page_score[pageid1]['category'] in CATEGORY and page_score[pageid2]['category'] in CATEGORY:
+                                edge_list.append(link)
+                            if (pageid1 not in node_exist):
+                                node_exist[pageid1]={}
+                            if (pageid2 not in node_exist):
+                                node_exist[pageid2]={}
+                            node_exist[pageid1][pageid2]=True
+                            node_exist[pageid2][pageid1]=True
+        g= nx.Graph()
+        degree={}
+        for page_id in page_score:
+            if month in page_score[page_id] and  len(page_score[page_id]['score'][month])>0 and page_score[page_id]['category'] in CATEGORY:
+                g.add_node(page_id)
+                degree[page_id]=0
+        for edge in edge_list:
+            if month in page_score[edge['source']] and len(page_score[edge['source']]['score'][month])>0 and page_score[edge['source']]['category'] in CATEGORY and month in page_score[edge['target']] and page_score[edge['target']]['category'] in CATEGORY:
+                g.add_edge(edge['source'],edge['target'],weight=edge['value'])
+        partition = community.best_partition(g)
+        if(len(g)>1):
+            degree = nx.degree_centrality(g)
+        betweenness = nx.betweenness_centrality(g)
+        # closeness = nx.closeness_centrality(g)
+        # eigenvector = nx.eigenvector_centrality(g)
+
+        for page_id in page_score:
+            if month in page_score[page_id] and  len(page_score[page_id]['score'][month])>0 and page_score[page_id]['category'] in CATEGORY:
+                catetory_id = page_score[page_id]['category']
+                if catetory_id==2 or catetory_id==4 or catetory_id==5:
+                    catetory_id=2
+                node = {
+                    'id':page_id  
+                }
+                node_list.append(node)
+                if page_id not in page_info_data:
+                     page_info_data[page_id]={ 'group':catetory_id,'name':page_score[page_id]['name']}
+                page_info_data[page_id][month]={
+                    'partition':partition[page_id],
+                    'group':catetory_id,
+                    'betweenness':betweenness[page_id],
+                    # 'closeness':closeness[page_id],
+                    # 'eigenvector':eigenvector[page_id],
+                    'degree':degree[page_id],
+                    'brand':page_score[page_id][month]['brand'],
+                    'media':page_score[page_id][month]['media'],
+                    'artist':page_score[page_id][month]['artist']
+                }
+
+        if month not in data['nodes']:
+            data['nodes'][month]=[]
+            data['links'][month]=[]
+        data['nodes'][month] = node_list
+        data['links'][month] = edge_list
+        # data={
+        #     'nodes':node_list,
+        #     'links':edge_list,
+        #     'page_info':page_info_data,
+        #     'category':list(set(category_data))
+        # }
+    data['page_info'] = page_info_data
     return data
 def main_generate_network_of_top():
     mongo = MongoDBManager('PageCollab')
     page_dict=mongo.get_all_from_collection('page_dict_monthly')[0]
     del page_dict['_id']
-    page_score_list = mongo.get_all_from_collection('page_score')
-    all_page_pairwise_list = mongo.get_all_from_collection('all_page_pairwise')
+    page_score_list = mongo.get_all_from_collection('page_score_monthly')
     page_score = extract_top_from_each_category(page_score_list)
     data = generate_data_of_top(page_score)
-    file_name = './view/datatop5 cat'+str(CATEGORY).replace('\'','').replace('[','').replace(']','').replace(', ','')+'.json'
+    file_name = './view/datatop5_monthly cat'+str(CATEGORY).replace('\'','').replace('[','').replace(']','').replace(', ','')+'.json'
     write_json(file_name,data)
 if __name__=="__main__":
     print('begin')
-    main_run_jaccard_single_call()
+    main_generate_network_of_top()
     print('done')
